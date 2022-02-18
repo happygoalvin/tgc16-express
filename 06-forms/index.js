4f1f18c97be57bcaae5a028dbb2241f7f72fe5f0
happygoalvin <yo.alvin.96@gmail.com>
2022-02-18 02:47:08 +0000

    completed hands on

--- 06-forms/index.js
+++ 06-forms/index.js
@@ -23,6 +23,19 @@ app.get('/add-food', function(req,res){
     res.render('add-food')
 })
 
+app.get('/calculate_bmi', function(req,res){
+    res.render('calculate_bmi')
+})
+
+app.post('/calculate_bmi', function(req,res){
+    let weight = parseFloat(req.body.weight);
+    let height = parseFloat(req.body.height);
+    calculateBmi = weight / height ** 2
+    res.render('results',{
+    'calculateBmi': calculateBmi
+    })
+});
+
 // intercept the data of the form
 // that is render at GET /add-food
 app.post('/add-food', function(req,res){
@@ -30,8 +43,54 @@ app.post('/add-food', function(req,res){
     res.send("Form recieved");
 })
 
+app.get('/fruits', function(req, res){
+    res.render('fruits')
+})
+
+app.post('/fruits', function(req,res){
+    let fruits = [];
+    if(req.body.items) {
+        if (Array.isArray(req.body.items)) {
+            fruits = req.body.items;
+        } else {
+            fruits = [ req.body.items ]
+        }
+    }
+    // METHOD 1
+    // if (fruits.includes('apple')) {
+    //     cost += 3;
+    // }
+    // if (fruits.includes('durian')) {
+    //     cost += 15;
+    // }
+    // if (fruits.includes('orange')) {
+    //     cost += 6
+    // }
+    // if (fruits.includes('banana')) {
+    //     cost += 4;
+    // }
+
+    let cost = 0;
+    let priceChart = {
+        'durian':15,
+        'apple':3,
+        'orange':6,
+        'banana':4,
+    };
+    // METHOD 2
+    // for (let f of fruits) {
+    //     cost += priceChart[f]
+    // }
+
+    // METHOD 3
+    cost = fruits.reduce(function(previous,current){
+        return previous+ priceChart[current]
+    }, 0);
+    res.send("Total cost is" + cost);
+})
+
+
 // LISTEN
 app.listen(3000, function(){
     console.log("Server has started")
 })
-
--- 06b-form-validation/index.js
+++ 06b-form-validation/index.js
@@ -2,4 +2,106 @@ const express = require('express');
 const hbs = require('hbs');
 const waxOn = require('wax-on');
 
+const app = express();
+app.set('view engine', 'hbs');
 
+waxOn.on(hbs.handlebars);
+waxOn.setLayoutPath('./views/layouts');
+
+//enable forms
+app.use(express.urlencoded({
+    extended:false
+}))
+
+app.get('/', function(req,res){
+    res.send("it's alive")
+});
+
+app.get('/login', function(req,res){
+    res.render('login')
+})
+
+app.post('/login', function(req,res){
+    //validation rules:
+    //1. email must not be empty and containes at least one @
+    //2. password must not be empty and contains at least three characters
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let hasError = false;
+
+    // if the email is null, undefined or empty string. then we report the error
+    if (!email) {
+        hasError = true;
+    }
+    // make sure email is not undefined or empty string
+    // make use of logical short circuit
+    if (email && !email.includes('@')) {
+        hasError = true;
+    }
+
+    // make sure the password is not undefined or empty string first,
+    // then make sure it has at least three characters.
+    if (password && password.length < 3) {
+        hasError = true;
+    }
+    
+    if (hasError) {
+        res.sendStatus(418);
+    } else {
+        res.send('success');
+    }
+})
+
+app.get('/lostnfound', function(req,res){
+    res.render('lostnfound');
+})
+
+app.post('/lostnfound', function(req,res){
+    let item = req.body.item;
+    let email = req.body.email;
+    let location = req.body.location;
+    let properties = []
+    if (req.body.properties) {
+       if (Array.isArray(req.body.properties)){
+        properties = req.body.properties;
+       } else {
+        properties = [ req.body.properties ];
+       }
+}
+
+    let hasError = false;
+
+    if(!item) {
+        hasError = true;
+    }
+
+    if (item && item.length > 200 || item.length < 3){
+        hasError = true;
+    }
+
+    if (!email) {
+        hasError = true;
+    }
+    if (email && !email.includes('@') && !email.includes('.')){
+        hasError = true;
+    }
+
+    if (!location) {
+        hasError = true;
+    }
+
+    if (properties && (properties.length < 1 || properties.length > 3)){
+        hasError = true;
+    }
+    
+    if (hasError) {
+        res.sendStatus(418);
+    } else {
+        res.send('success');
+    }
+})
+
+app.listen(3000, function(){
+    console.log("Server has started");
+});
